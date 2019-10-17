ba791b9e91439aa6236a7c12d7f65badaf556784
dcais <dyingjie_cai@163.com>
2019-10-17 15:25:33 +0800

    commit

--- src/pick.py
+++ src/pick.py
@@ -4,7 +4,7 @@ from os.path import abspath, join, dirname
 import pandas as pd
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S %p')
 sys.path.insert(0, join(abspath(dirname(__file__)), '..'))
 
 from src.stock import Stock
